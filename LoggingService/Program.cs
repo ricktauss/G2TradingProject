8ab87f02b3a3b59de2cd58060c729266a20b576a
Rick Tauss <tauss.rick@gmail.com>
2023-06-17 15:22:53 +0200

    feat(log): add NLog as logging provider to log to file

--- LoggingService/Program.cs
+++ LoggingService/Program.cs
@@ -1,7 +1,8 @@
 
 using LoggerService.Services;
-using Microsoft.Extensions.Options;
 using System.Reflection;
+using NLog;
+using NLog.Web;
 
 namespace LoggerService
 {
@@ -9,44 +10,67 @@ namespace LoggerService
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            // Early init of NLog to allow startup and exception logging, before host is built
+            var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            logger.Debug("init main");
 
-            // Add services to the container.
-            builder.Services.AddSingleton<ILoggingService, LoggingService>();
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
+            try
             {
-                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-            });
+                var builder = WebApplication.CreateBuilder(args);
 
-            builder.WebHost.ConfigureKestrel(serverOptions =>
-            {
-                serverOptions.ConfigureEndpointDefaults(listenOptions =>
+                // NLog: Setup NLog for Dependency injection
+                builder.Logging.ClearProviders();
+                builder.Host.UseNLog();
+
+                // Add services to the container.
+                builder.Services.AddSingleton<ILoggingService, LoggingService>();
+
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen(options =>
                 {
-                    // ...
+                    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
                 });
-            });
 
-            var app = builder.Build();
+                builder.WebHost.ConfigureKestrel(serverOptions =>
+                {
+                    serverOptions.ConfigureEndpointDefaults(listenOptions =>
+                    {
+                        // ...
+                    });
+                });
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                //if (app.Environment.IsDevelopment())
+                //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            //}
+                //}
 
-            app.UseHttpsRedirection();
+                app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+                app.UseAuthorization();
 
 
-            app.MapControllers();
+                app.MapControllers();
 
-            app.Run();
+                app.Run();
+            }
+            catch (Exception exception)
+            {
+                // NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
+
         }
     }
 }
