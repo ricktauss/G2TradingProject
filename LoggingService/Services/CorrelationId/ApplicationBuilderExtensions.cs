34c7964fbdd8fd9e77ec7e6485142269e89c28f8
ricktauss <57318706+ricktauss@users.noreply.github.com>
2023-06-28 09:05:07 +0200

    feat(log): add correlation ID handling for better traceability (#5)
    * feat(log): add correlation ID handling for better traceability
    
    * bugfix(log): fixed naming of correlation id header to standard

---
+++ LoggingService/Services/CorrelationId/ApplicationBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggerService.Services.CorrelationId
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder AddCorrelationIdMiddleware(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<CorrelationIdMiddleware>();
+        }
+    }
+}
---
+++ LoggingService/Services/CorrelationId/CorrelationIdGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggerService.Services.CorrelationId
+{
+    public class CorrelationIdGenerator : ICorrelationIdGenerator
+    {
+        private string _correlationId = Guid.NewGuid().ToString();
+
+        public string Get() => _correlationId;
+        public void Set(string correlationId) => _correlationId = correlationId;
+    }
+}
