208d15c3ececac6bc2deedb16dae7cc057e65fed
prezi1 <Patrick.Prettenhofer@edu.campus02.at>
2023-07-03 20:18:24 +0200

    Controller and Service for KeyValue in Consul created

---
+++ ConsulKeyValueService/Controllers/ServiceKeyValueController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Consul;
+using ConsulKeyValueService.Services;
+using Newtonsoft.Json.Linq;
+
+namespace ConsulKeyValueService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceKeyValueController : ControllerBase
+    {
+
+        private readonly KeyValueService _keyValueService;
+
+        public ServiceKeyValueController(KeyValueService keyValueService) {
+
+            _keyValueService  = keyValueService;
+
+        }
+
+
+        [HttpGet("{key}")]
+        public async Task<ActionResult> GetKeyValue(string key)
+        {
+            ServiceResult<string> serviceResult = await _keyValueService.GetKeyValue(key);
+
+            if (serviceResult.Success)
+                return Ok(serviceResult);
+            return BadRequest(serviceResult.ErrorMessage);
+        }
+
+
+        [HttpPost("{key}")]
+
+        public async Task<ActionResult> PostKeyValue(string key, [FromBody] string value)
+        {
+
+           ServiceResult<string> serviceResult = await _keyValueService.SetKeyValue(key, value);
+
+            if (serviceResult.Success)
+                return Ok("Key has beed added successfully to Consul");
+            return BadRequest(serviceResult.ErrorMessage);
+
+        }
+
+    }
+}
---
+++ ConsulKeyValueService/Services/KeyValueService.cs
@@ -0,0 +1,97 @@
+﻿using Consul;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ConsulKeyValueService.Services
+{
+    public class KeyValueService
+    {
+
+        private Uri _consulServerUrl;
+        private readonly ConsulClient _consulClient;
+
+
+
+        public KeyValueService(IConfiguration configuration)
+        {
+            _consulServerUrl = new Uri(configuration.GetValue<string>("ServiceDiscovery:Url"));
+            _consulClient = new ConsulClient(c => c.Address = _consulServerUrl);
+
+        }
+
+
+        public async Task<ServiceResult<string>> GetKeyValue (string key)
+        {
+            try
+            {
+
+            var getPair = await _consulClient.KV.Get(key);
+
+            if(getPair.Response == null)
+            {
+                    return new ServiceResult<string>
+                    {
+                        Success = false,
+                        ErrorMessage = "Key not found in Consul!"
+                    };
+            }
+
+            var secretValue = Encoding.UTF8.GetString(getPair.Response.Value, 0, getPair.Response.Value.Length);
+
+                return new ServiceResult<string>
+                {
+                    Success = true,
+                    Data = secretValue
+                };
+
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<string>
+                {
+                    Success = false,
+                    ErrorMessage = $"An error occured while trying to get the KeyValue pair!: {ex.Message}"
+                };
+            }
+
+        }
+
+
+
+        public async Task<ServiceResult<string>> SetKeyValue (string key, string value)
+        {
+            try
+            {
+
+            var putPair = new KVPair(key);
+            putPair.Value = Encoding.UTF8.GetBytes(value);
+
+            var putAttempt = await _consulClient.KV.Put(putPair);
+
+            if (putAttempt.Response)
+            {
+                    return new ServiceResult<string>
+                    {
+                        Success = false,
+                        ErrorMessage = "Failed to add Key Value Par to Consul!"
+                    };
+             }
+
+                return new ServiceResult<string>
+                {
+                    Success = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<string>
+                {
+                    Success = false,
+                    ErrorMessage = $"An error occured while trying to set the keyValue pair!: {ex.Message}"
+                };
+            }
+        }
+
+    }
+}
