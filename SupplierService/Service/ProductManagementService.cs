0c643be0e3168d9d1a75c0dc4cb7fd4de45b1e79
prezi1 <Patrick.Prettenhofer@edu.campus02.at>
2023-07-04 21:26:06 +0200

    New Service for Sullpier

---
+++ SupplierService/Service/ProductManagementService.cs
@@ -0,0 +1,40 @@
+﻿namespace SupplierService.Service
+{
+    public class ProductManagementService
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+
+        public ProductManagementService(IConfiguration configuration, IHttpClientFactory clientFactory) {
+
+            _configuration = configuration;
+            _httpClientFactory = clientFactory;
+
+        }
+
+
+        public async Task<string> PostNewProduct(string uri, string description)
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Post, uri);
+            request.Content = new StringContent(description);
+
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode) 
+            { 
+                return await response.Content.ReadAsStringAsync();  
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+
+    }
+}
