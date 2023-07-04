b8c346d9839335d1f562a2df4a8dc05e6f3bba78
Rick Tauss <tauss.rick@gmail.com>
2023-07-04 23:48:15 +0200

    build: add deployment batch file

---
+++ StartDeployment.bat
@@ -0,0 +1,14 @@
+REM ###Deklaration von Variablen###
+set "base_path=%~dp0bin\Debug\net7.0\"
+
+REM ###Deklaration von Environment Variablen die in Services verwendet werden
+set "CONSUL_SERVER_URL=http://localhost:8500"
+
+
+REM ###Starten von ConsulServiceDiscovery CreditCardService-Sidecar###
+set "CREDITCARD_SERVICE_DISCOVERY_URL=https://localhost:5029"
+start "" cmd /k dotnet "%base_path%ConsulServiceDiscovery.dll" --urls="%CREDITCARD_SERVICE_DISCOVERY_URL%"
+
+REM ###Starten von CreditcardService Instanzen###
+start "" cmd /k dotnet "%base_path%CreditcardService.dll" --urls="https://localhost:5020;http://localhost:5025"
+start "" cmd /k dotnet "%base_path%CreditcardService.dll" --urls="https://localhost:5021;http://localhost:5026"
