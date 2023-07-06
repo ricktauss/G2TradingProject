e3e2fa3f851bd6fd5bdbf6dbdcd3cdf6cbbe3a0d
prezi1 <Patrick.Prettenhofer@edu.campus02.at>
2023-07-06 17:49:13 +0200

    HealthCheckController integrated in all relevant services.

---
+++ ConsulKeyValueService/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsulKeyValueService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("")]
+        [HttpHead("")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+
+    }
+}
